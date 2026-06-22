d7263364274cf2e9beaad13620f748890aada93e
inzamulhaque <mdihalif@gmail.com>
2026-06-22 18:11:49 +0600

    feat(form): add HNForm component

---
+++ components/shared/form/HNForm.tsx
@@ -0,0 +1,48 @@
+import { ReactNode } from "react";
+import {
+  FieldValues,
+  FormProvider,
+  Resolver,
+  SubmitHandler,
+  useForm,
+} from "react-hook-form";
+
+type TFormConfig = {
+  resolver?: Resolver<FieldValues>;
+  defaultValues?: Record<string, any>;
+};
+
+type TFormProps = {
+  children: ReactNode;
+  submit: SubmitHandler<FieldValues>;
+} & TFormConfig;
+
+const HNForm = ({ children, submit, resolver, defaultValues }: TFormProps) => {
+  const formConfig: TFormConfig = {};
+
+  if (resolver) {
+    formConfig["resolver"] = resolver;
+  }
+
+  if (defaultValues) {
+    formConfig["defaultValues"] = defaultValues;
+  }
+
+  const methods = useForm(formConfig);
+  const { handleSubmit, reset } = methods;
+
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
+    submit(data);
+    reset();
+  };
+
+  return (
+    <>
+      <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(onSubmit)}>{children}</form>
+      </FormProvider>
+    </>
+  );
+};
+
+export default HNForm;
